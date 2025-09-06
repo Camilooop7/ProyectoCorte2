21682c098058d3857ba16293395a3a6c346764e6
Camilooop7 <camilooos.p7@gmail.com>
2025-09-05 22:14:37 -0500

    Ya cree el sprinboot

---
+++ proyectocorte2/src/main/java/co/edu/unbosque/proyectocorte2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.proyectocorte2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Proyectocorte2Application.class);
+	}
+
+}
---
+++ proyectocorte2/src/test/java/co/edu/unbosque/proyectocorte2/Proyectocorte2ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.proyectocorte2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectocorte2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
